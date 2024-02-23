0eaf749ff889dc6070cc28e8957ea76c67631d03
YuvarajPatil1304 <87464910+YuvarajPatil1304@users.noreply.github.com>
2024-02-23 16:38:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+dotenv.config()
+import { UserRouter } from './routes/user.js'
+
+const app = express()
+app.use(express.json())
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    credentials: true
+}))
+app.use(cookieParser())
+app.use('/auth',UserRouter)
+
+mongoose.connect('mongodb+srv://1rn18cs090saiyuvarajpatil:Ljyd1234@cluster0.tpdwfpi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is running")
+})
