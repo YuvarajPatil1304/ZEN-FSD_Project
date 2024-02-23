9ef1347c126a7a8071acde5f9ef43eec18424048
YuvarajPatil1304 <87464910+YuvarajPatil1304@users.noreply.github.com>
2024-02-24 02:53:03 +0530

    Update ResetPassword.jsx

--- src/components/ResetPassword.jsx
+++ src/components/ResetPassword.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import '../App.css'
 import Axios from 'axios'
 import { useNavigate, Link, useParams } from 'react-router-dom';
+const BASE_URL = process.env.BASE_URL;
 
 function ResetPassword() {
     const [password, setPassword] = useState('');
@@ -11,7 +12,7 @@ function ResetPassword() {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        Axios.post('https://zen-fsd-project-backend.onrender.com/auth/resetPassword/'+token, {
+        Axios.post(`${BASE_URL}/auth/resetPassword/`+token, {
             password, 
         }).then(response => {
             if(response.data.status){
