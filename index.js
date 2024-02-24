e4e7b0fd6aba45c846348792926547452dce0e6e
YuvarajPatil1304 <87464910+YuvarajPatil1304@users.noreply.github.com>
2024-02-24 16:19:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,19 +6,16 @@ import cookieParser from 'cookie-parser'
 dotenv.config()
 import { UserRouter } from './routes/user.js'
 
-const BASE_URL = process.env.BASE_URL;
-const MONGO_URL = process.env.MONGO_URL;
-
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: [`${BASE_URL}`],
+    origin: ['https://zen-fsd-project-backend.onrender.com/'],
     credentials: true
 }))
 app.use(cookieParser())
 app.use('/auth',UserRouter)
 
-mongoose.connect(`${MONGO_URL}`)
+mongoose.connect('mongodb+srv://1rn18cs090saiyuvarajpatil:Ljyd1234@cluster0.tpdwfpi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
 
 app.listen(process.env.PORT, () => {
