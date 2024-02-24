938f2dc96a705afb749398b24791028aae1e550f
YuvarajPatil1304 <87464910+YuvarajPatil1304@users.noreply.github.com>
2024-02-24 16:34:24 +0530

    Update MarkdownPage.jsx

--- src/components/MarkdownPage.jsx
+++ src/components/MarkdownPage.jsx
@@ -3,13 +3,12 @@ import '../App.css'
 import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Markdown from './Markdown/Markdown_Viewer/Markdown'
-const BASE_URL = process.env.BASE_URL;
 
 function MarkdownPage() {
     const navigate = useNavigate()
     axios.defaults.withCredentials = true;
     useEffect(() => {
-        axios.get(`${BASE_URL}/auth/verify`)
+        axios.get(https://zen-fsd-project-backend.onrender.com/auth/verify')
         .then(res => {
             if(res.data.status){
 
