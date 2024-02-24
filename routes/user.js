ed6e6200edeb6bd8362a0c177d98b6f583ccc4cf
YuvarajPatil1304 <87464910+YuvarajPatil1304@users.noreply.github.com>
2024-02-24 16:17:44 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -5,8 +5,6 @@ import { User } from '../models/User.js'
 import jwt from 'jsonwebtoken'
 import nodemailer from 'nodemailer'
 
-const BASE_URL = process.env.BASE_URL;
-
 router.post('/signup', async (req, res) => {
     const { username, email, password } = req.body;
     const user = await User.findOne({ email })
@@ -58,7 +56,7 @@ router.post('/forgotPassword', async (req, res) => {
             from: '1rn18cs090.saiyuvarajpatil@gmail.com',
             to: email,
             subject: 'Reset Password',
-            text: `${BASE_URL}/resetPassword/${token}`
+            text: 'https://zen-fsd-project-backend.onrender.com/resetPassword/${token}'
         };
 
         transporter.sendMail(mailOptions, function (error, info) {
